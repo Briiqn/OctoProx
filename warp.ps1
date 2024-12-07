4b43518cf515943b1f522e434dac5e18886511f5
Briiqn <82249811+Briiqn@users.noreply.github.com>
2024-12-07 16:31:35 -0500

    Update warp.ps1

--- warp.ps1
+++ warp.ps1
@@ -42,8 +42,7 @@ function Connect-WireGuard {
     $currentDirectory = Get-Location
     Write-Host "Current Directory: $currentDirectory" -ForegroundColor Magenta
     $wireguardPath = "C:\Program Files\WireGuard\wireguard.exe"
-    & "$wireguardPath" /installtunnelservice "D:\a\OctoProx\OctoProx\wgcf-profile.conf"
-    Start-Sleep -Seconds 2
+    & "$wireguardPath" /installtunnelservice "wgcf-profile.conf"
     Write-Host "Checking new IP address..." -ForegroundColor Green
     $newIP = (Invoke-RestMethod -Uri "http://ifconfig.me/ip").Trim()
     Write-Host "New IP Address: $newIP" -ForegroundColor Cyan
